2a4322d9edd99e14b47f79c5b84b62063f44d322
vlad u. <itvalidol@gmail.com>
2024-02-29 14:22:01 +0300

    gameviewmodel release

--- app/src/main/java/com/example/numbercomposition/domain/entity/Question.kt
+++ app/src/main/java/com/example/numbercomposition/domain/entity/Question.kt
@@ -1,9 +1,12 @@
 package com.example.numbercomposition.domain.entity
 
-data class Question (
-    val sum:Int,
-    val visibleNumber:Int,
-    val options:List<Int>
-)
+data class Question(
+    val sum: Int,
+    val visibleNumber: Int,
+    val options: List<Int>
+) {
+    val rightAnswer: Int
+        get() = sum - visibleNumber
+}
 
 
